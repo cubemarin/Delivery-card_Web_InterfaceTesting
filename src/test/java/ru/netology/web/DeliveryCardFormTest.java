e1edd1fefbf13daa93f06eb36cd988c12665186d
cubemarin <m.v.kubynina@gmail.com>
2022-11-29 22:33:55 +0700

    test added

---
+++ src/test/java/ru/netology/web/DeliveryCardFormTest.java
@@ -0,0 +1,37 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryCardFormTest {
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    @Test
+    void shouldCheckSendingForm() {
+        String date = generateDate(3);
+        open("http://localhost:9999");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=city] input").setValue("Новосибирск");
+        form.$("[data-test-id=date] input").sendKeys(Keys.chord(Keys.CONTROL, "A"), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(date);
+        form.$("[data-test-id=name] input").setValue("Миклухо-Маклай Николай");
+        form.$("[data-test-id=phone] input").setValue("+79999999999");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button").click();
+        $(".notification__title").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__title").shouldHave(exactText("Успешно!"));
+    }
+}
